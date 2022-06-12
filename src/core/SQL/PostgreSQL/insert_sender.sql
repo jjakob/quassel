jjakob/quassel

commit 20f446a492d8e681156423f0dc3637db78c45bae

Implement sender realname/avatarurl storage
- Store sender realname and avatarurl in database
- Guard protocol changes behind feature flag 'RichMessages'
- Avatar URLs are not actually stored yet, as we do not implement
  METADATA yet.

This is not yet used in Quassel itself, but third-party clients
including QuasselDroid use this to provide more context in messages
and allow for realname-based fallback avatar fetching to work in the
chat message backlog.

Incorporates suggestions from @mamarley.

Closes GH-351.

src/core/SQL/PostgreSQL/insert_sender.sql
@@ -1,3 +1,3 @@
-INSERT INTO sender (sender)
-VALUES ($1)
+INSERT INTO sender (sender, realname, avatarurl)
+VALUES ($1, $2, $3)
 RETURNING senderid

src/core/SQL/PostgreSQL/migrate_write_sender.sql
@@ -1,2 +1,2 @@
-INSERT INTO sender (senderid, sender)
-VALUES (?, ?)
+INSERT INTO sender (senderid, sender, realname, avatarurl)
+VALUES (?, ?, ?, ?)

src/core/SQL/PostgreSQL/select_senderid.sql
@@ -1,3 +1,3 @@
 SELECT senderid
 FROM sender
-WHERE sender = $1
+WHERE sender = $1 AND realname = $2 AND avatarurl = $3

src/core/SQL/PostgreSQL/setup_010_sender.sql
@@ -1,4 +1,6 @@
 CREATE TABLE sender ( -- THE SENDER OF IRC MESSAGES
        senderid serial NOT NULL PRIMARY KEY,
-       sender varchar(128) UNIQUE NOT NULL
-)
+       sender varchar(128) NOT NULL,
+       realname TEXT,
+       avatarurl TEXT
+);

src/core/SQL/SQLite/insert_sender.sql
@@ -1,2 +1,2 @@
-INSERT INTO sender (sender)
-VALUES (:sender)
+INSERT INTO sender (sender, realname, avatarurl)
+VALUES (:sender, :realname, :avatarurl)

src/core/SQL/SQLite/setup_010_sender.sql
@@ -1,6 +1,6 @@
 CREATE TABLE sender ( -- THE SENDER OF IRC MESSAGES
        senderid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-       sender TEXT UNIQUE NOT NULL
-)
-
-	  
+       sender TEXT NOT NULL,
+       realname TEXT,
+       avatarurl TEXT
+);

src/common/message.cpp
@@ -26,24 +26,31 @@
 
 #include <QDataStream>
 
-Message::Message(const BufferInfo &bufferInfo, Type type, const QString &contents, const QString &sender, const QString &senderPrefixes, Flags flags)
+Message::Message(const BufferInfo &bufferInfo, Type type, const QString &contents, const QString &sender,
+                 const QString &senderPrefixes, const QString &realName, const QString &avatarUrl, Flags flags)
     : _timestamp(QDateTime::currentDateTime().toUTC()),
     _bufferInfo(bufferInfo),
     _contents(contents),
     _sender(sender),
     _senderPrefixes(senderPrefixes),
+    _realName(realName),
+    _avatarUrl(avatarUrl),
     _type(type),
     _flags(flags)
 {
 }
 
 
-Message::Message(const QDateTime &ts, const BufferInfo &bufferInfo, Type type, const QString &contents, const QString &sender, const QString &senderPrefixes, Flags flags)
+Message::Message(const QDateTime &ts, const BufferInfo &bufferInfo, Type type, const QString &contents,
+                 const QString &sender, const QString &senderPrefixes, const QString &realName,
+                 const QString &avatarUrl, Flags flags)
     : _timestamp(ts),
     _bufferInfo(bufferInfo),
     _contents(contents),
     _sender(sender),
     _senderPrefixes(senderPrefixes),
+    _realName(realName),
+    _avatarUrl(avatarUrl),
     _type(type),
     _flags(flags)
 {
@@ -71,6 +78,11 @@ QDataStream &operator<<(QDataStream &out, const Message &msg)
 
     if (SignalProxy::current()->targetPeer()->hasFeature(Quassel::Feature::SenderPrefixes))
         out << msg.senderPrefixes().toUtf8();
+
+    if (SignalProxy::current()->targetPeer()->hasFeature(Quassel::Feature::RichMessages)) {
+        out << msg.realName().toUtf8();
+        out << msg.avatarUrl().toUtf8();
+    }
 
     out << msg.contents().toUtf8();
     return out;
@@ -113,6 +125,15 @@ QDataStream &operator>>(QDataStream &in, Message &msg)
         in >> senderPrefixes;
     msg._senderPrefixes = QString::fromUtf8(senderPrefixes);
 
+    QByteArray realName;
+    QByteArray avatarUrl;
+    if (SignalProxy::current()->sourcePeer()->hasFeature(Quassel::Feature::RichMessages)) {
+        in >> realName;
+        in >> avatarUrl;
+    }
+    msg._realName = QString::fromUtf8(realName);
+    msg._avatarUrl = QString::fromUtf8(avatarUrl);
+
     QByteArray contents;
     in >> contents;
     msg._contents = QString::fromUtf8(contents);
@@ -126,7 +147,9 @@ QDebug operator<<(QDebug dbg, const Message &msg)
     dbg.nospace() << qPrintable(QString("Message(MsgId:")) << msg.msgId()
     << qPrintable(QString(",")) << msg.timestamp()
     << qPrintable(QString(", Type:")) << msg.type()
+    << qPrintable(QString(", RealName:")) << msg.realName()
+    << qPrintable(QString(", AvatarURL:")) << msg.avatarUrl()
     << qPrintable(QString(", Flags:")) << msg.flags() << qPrintable(QString(")"))
-    << msg.sender() << ":" << msg.contents();
+    << msg.senderPrefixes() << msg.sender() << ":" << msg.contents();
     return dbg;
 }

src/common/message.h
@@ -68,10 +68,11 @@ public:
     Q_DECLARE_FLAGS(Flags, Flag)
 
     Message(const BufferInfo &bufferInfo = BufferInfo(), Type type = Plain, const QString &contents = {},
-            const QString &sender = {}, const QString &senderPrefixes = {}, Flags flags = None);
+            const QString &sender = {}, const QString &senderPrefixes = {}, const QString &realName = {},
+            const QString &avatarUrl = {}, Flags flags = None);
     Message(const QDateTime &ts, const BufferInfo &buffer = BufferInfo(), Type type = Plain,
             const QString &contents = {}, const QString &sender = {}, const QString &senderPrefixes = {},
-            Flags flags = None);
+            const QString &realName = {}, const QString &avatarUrl = {}, Flags flags = None);
 
     inline static Message ChangeOfDay(const QDateTime &day) { return Message(day, BufferInfo(), DayChange); }
     inline const MsgId &msgId() const { return _msgId; }
@@ -83,6 +84,8 @@ public:
     inline const QString &contents() const { return _contents; }
     inline const QString &sender() const { return _sender; }
     inline const QString &senderPrefixes() const { return _senderPrefixes; }
+    inline const QString &realName() const { return _realName; }
+    inline const QString &avatarUrl() const { return _avatarUrl; }
     inline Type type() const { return _type; }
     inline Flags flags() const { return _flags; }
     inline void setFlags(Flags flags) { _flags = flags; }
@@ -99,6 +102,8 @@ private:
     QString _contents;
     QString _sender;
     QString _senderPrefixes;
+    QString _realName;
+    QString _avatarUrl;
     Type _type;
     Flags _flags;
 

commit d3043f961fd7f6cabcc9045e4977f3e69deea330

Fix sender table having a lot of duplicated senders (bug 1245)
Commit 20f446a4 introduced a bug, where the unique index on sender
(sender, realname, avatarurl) did not catch inserts where realname or
avatarurl were NULL, because the columns lacked the NOT NULL constraint.
Unique indexes do not consider NULL equal to NULL.

This means a lot of new senderid's were created (one for each insert in
the sender table) where one of these values was null, which bloated the DB
unnecessarily, and may have caused other bad behavior.

This commit:
- adds NOT NULL constraints on the sender table realname and avatarurl columns
- changes the sender insert statements to coalesce the value with an
  empty string (otherwise there are errors where it attempts to insert
  NULL)
- adds upgrade scripts

The upgrade scripts are quite complex:
- first they drop the unique index on sender
- update all null values on modified columns to empty string
- update the sender table schema to add NOT NULL constraints
- create a temp table that holds all rows that would violate the unique
  index, with good and bad senderid, that are used later
- triggers on backlog and sender tables are disabled or deleted,
  to speed up the operation
- backlog table bad senderid's are updated to good senderid's
- rows from sender with bad senderid's are deleted
- triggers are reenabled or recreated
- tables sender and backlog are reindexed
- unique index on sender is recreated

I opted to not put the statements into separate numbered files in the
upgrade directory, as they would get put into separate transactions,
which could leave the db in a hard to recover state if any of them are
unsuccessful. Better to leave the db untouched in the previous version.

src/core/SQL/PostgreSQL/insert_sender.sql
@@ -1,3 +1,3 @@
 INSERT INTO sender (sender, realname, avatarurl)
-VALUES ($1, $2, $3)
+VALUES (COALESCE($1, ''), COALESCE($2, ''), COALESCE($3,''))
 RETURNING senderid

src/core/SQL/PostgreSQL/migrate_write_sender.sql
@@ -1,2 +1,2 @@
 INSERT INTO sender (senderid, sender, realname, avatarurl)
-VALUES (?, ?, ?, ?)
+VALUES (?, COALESCE(?, ''), COALESCE(?, ''), COALESCE(?, ''))

src/core/SQL/PostgreSQL/select_senderid.sql
@@ -1,3 +1,3 @@
 SELECT senderid
 FROM sender
-WHERE sender = $1 AND coalesce(realname, '') = coalesce($2, '') AND coalesce(avatarurl, '') = coalesce($3, '')
+WHERE sender = coalesce($1, '') AND realname = coalesce($2, '') AND avatarurl = coalesce($3, '')

src/core/SQL/PostgreSQL/setup_010_sender.sql
@@ -1,6 +1,6 @@
 CREATE TABLE sender ( -- THE SENDER OF IRC MESSAGES
        senderid bigserial NOT NULL PRIMARY KEY,
        sender TEXT NOT NULL,
-       realname TEXT,
-       avatarurl TEXT
+       realname TEXT NOT NULL,
+       avatarurl TEXT NOT NULL
 );

src/core/SQL/PostgreSQL/version/32/upgrade_000_sender_fix_unique_index.sql
@@ -1,0 +1,57 @@
+/* do all the steps in one transaction, as a partially finished
+ * procedure may leave the db in a hard to recover state */
+DROP INDEX IF EXISTS sender_sender_realname_avatarurl_uindex;
+
+/* part 1: update columns */
+UPDATE sender
+SET realname=''
+WHERE realname IS NULL
+;
+
+UPDATE sender
+SET avatarurl=''
+WHERE avatarurl IS NULL
+;
+
+ALTER TABLE sender ALTER realname SET NOT NULL;
+ALTER TABLE sender ALTER avatarurl SET NOT NULL;
+
+/* part 2: get table of conflict rows */
+CREATE TEMP TABLE bad_senderids AS
+SELECT good_senderid, bad_senderid
+FROM (
+	SELECT min(senderid) OVER (
+			PARTITION BY
+				sender,
+				realname,
+				avatarurl
+		) AS good_senderid,
+		senderid AS bad_senderid
+	FROM sender
+) AS x
+WHERE bad_senderid > good_senderid
+;
+
+/* part 3: do the changes */
+ALTER TABLE backlog DISABLE TRIGGER ALL;
+ALTER TABLE sender DISABLE TRIGGER ALL;
+
+UPDATE backlog
+SET senderid=bsi.good_senderid
+FROM bad_senderids bsi
+WHERE backlog.senderid=bsi.bad_senderid
+;
+
+DELETE FROM sender
+USING bad_senderids bsi
+WHERE senderid = bsi.bad_senderid
+;
+
+ALTER TABLE backlog ENABLE TRIGGER ALL;
+ALTER TABLE sender ENABLE TRIGGER ALL;
+
+REINDEX TABLE sender;
+REINDEX TABLE backlog;
+
+CREATE UNIQUE INDEX sender_sender_realname_avatarurl_uindex
+ON sender USING btree (sender, realname, avatarurl);

src/core/SQL/SQLite/insert_message.sql
@@ -1,5 +1,5 @@
 INSERT INTO backlog (time, bufferid, type, flags, senderid, senderprefixes, message)
 VALUES (:time, :bufferid, :type, :flags,
-	(SELECT senderid FROM sender WHERE sender = :sender AND coalesce(realname, '') = coalesce(:realname, '') AND coalesce(avatarurl, '') = coalesce(:avatarurl, '')),
+	(SELECT senderid FROM sender WHERE sender = coalesce(:sender, '') AND realname = coalesce(:realname, '') AND avatarurl = coalesce(:avatarurl, '')),
 	:senderprefixes, :message
 )

src/core/SQL/SQLite/insert_sender.sql
@@ -1,2 +1,2 @@
 INSERT INTO sender (sender, realname, avatarurl)
-VALUES (:sender, :realname, :avatarurl)
+VALUES (COALESCE($1, ''), COALESCE($2, ''), COALESCE($3,''))

src/core/SQL/SQLite/setup_010_sender.sql
@@ -1,6 +1,6 @@
 CREATE TABLE sender ( -- THE SENDER OF IRC MESSAGES
        senderid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
        sender TEXT NOT NULL,
-       realname TEXT,
-       avatarurl TEXT
+       realname TEXT NOT NULL,
+       avatarurl TEXT NOT NULL
 );

src/core/SQL/SQLite/version/33/upgrade_000_sender_fix_unique_index.sql
@@ -1,0 +1,75 @@
+/* do all the steps in one transaction, as a partially finished
+ * procedure may leave the db in a hard to recover state */
+DROP INDEX IF EXISTS sender_index;
+
+/* part 1: update columns */
+UPDATE sender
+SET realname=''
+WHERE realname IS NULL
+;
+
+UPDATE sender
+SET avatarurl=''
+WHERE avatarurl IS NULL
+;
+
+/* we can't just change the column constraint like in PostgreSQL
+ * so we need to create a new table */
+CREATE TABLE sender_new (
+       senderid INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+       sender TEXT NOT NULL,
+       realname TEXT NOT NULL,
+       avatarurl TEXT NOT NULL
+);
+
+INSERT INTO sender_new SELECT * FROM sender;
+
+DROP TABLE sender;
+
+ALTER TABLE sender_new RENAME TO sender;
+
+/* part 2: get table of conflict rows */
+CREATE TEMP TABLE bad_senderids AS
+SELECT good_senderid, bad_senderid
+FROM (
+	SELECT min(senderid) OVER (
+			PARTITION BY
+				sender,
+				realname,
+				avatarurl
+		) AS good_senderid,
+		senderid AS bad_senderid
+	FROM sender
+) AS x
+WHERE bad_senderid > good_senderid
+;
+
+/* part 3: do the changes */
+/* we can't disable triggers like on PostgreSQL */
+DROP TRIGGER backlog_lastmsgid_update_trigger_update;
+
+UPDATE backlog
+SET senderid=bsi.good_senderid
+FROM bad_senderids bsi
+WHERE backlog.senderid=bsi.bad_senderid
+;
+
+DELETE FROM sender
+USING bad_senderids bsi
+WHERE senderid = bsi.bad_senderid
+;
+
+CREATE TRIGGER IF NOT EXISTS backlog_lastmsgid_update_trigger_update
+AFTER UPDATE
+ON backlog
+FOR EACH ROW
+    BEGIN
+        UPDATE buffer
+        SET lastmsgid = new.messageid
+        WHERE buffer.bufferid = new.bufferid
+            AND buffer.lastmsgid < new.messageid;
+    END
+
+CREATE UNIQUE INDEX sender_index
+ON sender (sender, realname, avatarurl);
+/* SQLite setup does not have any foreign keys like PostgreSQL does ? */